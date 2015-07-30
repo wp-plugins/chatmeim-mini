8eed61164cc96463da2b32ef9a871bdcaebabf84
camaran <camaran@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 18:28:08 +0000

    other translatable string
    git-svn-id: https://plugins.svn.wordpress.org/chatmeim-mini/trunk@1209879 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class.shortcode.php
+++ classes/class.shortcode.php
@@ -17,7 +17,7 @@ class ShortCodes extends ChatMe {
 	}
 	
 	function chatme_shortcode_menu() {
-  		$my_admin_page = add_options_page('ChatMe Shortocode Help', 'ChatMe Shortcode Help', 'manage_options', $this->default['plugin_options_short'], array($this, 'mini_shortcode_help') );
+  		$my_admin_page = add_options_page( __('ChatMe Shortocode Help', 'chatmini'), __('ChatMe Shortcode Help', 'chatmini'), 'manage_options', $this->default['plugin_options_short'], array($this, 'mini_shortcode_help') );
 	}
 	
 function mini_shortcode_help() {
@@ -26,7 +26,7 @@ function mini_shortcode_help() {
   		} 
 	?>
  	<div class="wrap">
-	<h1>ChatMe Shortocode Help</h1>
+	<h1><?php _e('ChatMe Shortocode Help', 'chatmini'); ?></h1>
 	<p><?php _e('<b>[userStatus user="users" link=1 hosted=0]</b><br/>This code show user status (online/offline/etc):<ul><li><b>user</b>: insert the user with the domain (example: user@chatme.im)</li><li><b>link</b> (boolean): can be 0 (default) for not link and 1 for link to the user</li><li><b>hosted</b> (boolean): can be 0 (default) for not hosted domain and 1 if you have a custom domain hosted in ChatMe XMPP server</li></ul>', 'chatmini'); ?></p> 
 	<p><?php _e('<b>[chatRoom anon=1]</b><br/>This code show a list of default chat room.<ul><li><b>anon</b> (boolean): can be 0 for not anonymous login (require username and password) or 1 (default) for chat only with nickname.</li></ul>', 'chatmini'); ?></p> 
 	<p><?php _e('<b>[chatRoomIframe room="room" width="width" height="height" hosted=0]</b><br/>This shortcode show a chat room in your wordpress page:<ul><li><b>room</b>: the name of the chat room (default: piazza@conference.chatme.im)</li><li><b>width</b>: the frame width (default: 100%)</li><li><b>height</b>: the height of frame (default: 100%)</li><li><b>hosted</b> (boolean): can be 0 (default) for not hosted domain and 1 if you have a custom domain hosted in ChatMe XMPP server</li></ul>', 'chatmini'); ?></p> 
